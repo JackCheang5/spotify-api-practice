ebf60ebc89c11bebb3d413e21ad9f9f3890d300f
JackCheang5 <jackcheang5@gmail>
2021-12-15 23:20:25 +0800

    final build

---
+++ pages/_middleware.js
@@ -0,0 +1,22 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  const { pathname } = req.nextUrl;
+  // if already logged in, redirect to home
+  if (token && pathname === '/login') {
+    return NextResponse.redirect('/');
+  }
+  // allow req if the following is true
+  // 1. its a req for next-auth session & provider fetching
+  // 2. if token exists
+  if (pathname.includes('/api/auth') || token) {
+    return NextResponse.next();
+  }
+  // redirect them to login if no token AND reqesting a protected route
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect('/login');
+  }
+}
